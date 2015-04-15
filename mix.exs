7cefd83b2e50058e5792b5395e0f6e3c0fe61832
camshaft <bytheway.cameron@gmail.com>
2015-04-14 19:56:08 -0600

    release 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PoolRing.Mixfile do
 
   def project do
     [app: :pool_ring,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.0",
      description: "create a pool based on a hash ring",
      package: package,
