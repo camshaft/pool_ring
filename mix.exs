2fd1f1cf4b00a04bfd3bd1e7aff650d5a761d2cd
camshaft <bytheway.cameron@gmail.com>
2015-05-28 14:07:16 -0600

    release 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,30 +3,18 @@ defmodule PoolRing.Mixfile do
 
   def project do
     [app: :pool_ring,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      description: "create a pool based on a hash ring",
      package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger],
      mod:          {PoolRing, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
